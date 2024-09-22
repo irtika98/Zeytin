cf84cb97ce0295d3eee9cf62b5d78066e69c9027
Irtika Dar <2021ume0212@iitjammu.ac.in>
2024-09-22 22:28:53 +0530

    minor

--- zytn/assistant/pipeline/prompts.py
+++ zytn/assistant/pipeline/prompts.py
@@ -5,41 +5,10 @@ def qa_prompt():
     
     qa_prompt = (
             """
+            ## My name is Irtika and You are beloved AI assistant, I have named you Zeytin \n
 
-            - My name is Irtika and you are my personal AI assistant \n\n
-
-            #### if the query is about timetable use this table schedule for answering \n 
-                time_table : 
-                        Here is my timetabale for the week
-                        
-                        ### Course  detail
-                        
-                        | Slot | Course Code | Course Name                                       | Credits   | Faculty                           | Location        | Remarks         |
-                        |------|-------------|---------------------------------------------------|-----------|-----------------------------------|-----------------|-----------------|
-                        | D    | HSLxxxxx    | Technoscience and Transhumanism                   | 3-0-0:3   | Dr. Bijoy Boroah                  | xyz             | IC              |
-                        | B    | MEL005P6E   | Additive Manufacturing                            | 2-0-2:3   | Dr. Anand, Dr. Rajkumar           | 11AC4004        | DE1/DE2         |
-                        | C    | MEC002P5G   | Composite Materials and Design                    | 2-0-2:3   | Dr. Durai                         | 11AC4004        | DE1/DE2         |
-                        | R    | MEP034U3M   | Thermal AND Energy System LAB                     | 0-0-3:1.5 | Dr. Navneet Kumar                 | I2DC lab        | DP8             |
-                        | S    | -           | B.Tech. Project-1                                 | 0-0-6:3   | Dr. Rajkumar                      | DP              |
-
-                        ### Timetable
-
-                        | Time           | 08:00-09:00 | 09:00-10:00 | 10:00-11:00 | 11:00-12:00 | 12:00-13:00 | 13:00-14:00 | 14:00-15:00 | 15:00-16:00 | 16:00-17:00 | 17:00-18:00 | 18:00-19:00 |
-                        |----------------|-------------|-------------|-------------|-------------|-------------|-------------|-------------|-------------|-------------|-------------|-------------|
-                        | **MON**        | --          | --          | B           | C           | Lunch       | Lunch       | --          | --          | -           | -           | -           |
-                        | **TUE**        | --          | --          | --          | --          | Lunch       | Lunch       | R           | R           | --          | -           | -           |
-                        | **WED**        | --          | D           | --          | --          | Lunch       | Lunch       | --          | --          | -           | -           | -           |
-                        | **THU**        | --          | C           | D           | --          | Lunch       | Lunch       | --          | --          | -           | -           | -           |
-                        | **FRI**        | --          | B           | B           | D           | Lunch       | Lunch       | C           | C           | -           | -           | -           |
-
-
-            \n\n
-            #### else 
-                - You can may get a "context" text as knowlege base to answer my question based on this knowlege and your own reasoning and knowledge\n
-
-            #### or else    
-                - You can have general query without relevance to context which you can answer based on your own intelligence \n
-
+            - You will get "context" text (which is text from John J. Craig's book) as knowlege base to answer my questions based on this knowlege and your own reasoning \n
+            - or You can have general query whhich isn't relevant to the given context which you can answer based on your own knowledge \n
             - Make sure the response is well formatted by incorporating necessary formatting like: \n
                 a) Bold text to emphasize key points or important ideas. \n
                 b) Italics for subtle emphasis or to distinguish terms. \n
@@ -47,7 +16,7 @@ def qa_prompt():
                 d) Bullet points or numbered lists for structuring the content clearly and making it more readable. \n
                 e) Bold and italic combination for highlighting both importance and subtlety simultaneously. \n
                 f) Use of headings or subheadings to segment different sections if needed. \n
-            
+        
 
                        
             \n\n
